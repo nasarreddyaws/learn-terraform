40ba3743334a7ee967d13cd67f3e14fe6cead5be
Nasar Reddy <nasarreddyaws@gmai.com>
2023-03-05 16:20:07 +0530

    test.tf Ec2 without tag name

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,8 +1,5 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-05475defcc5748318"]
-  tags = {
-    Name = "test"
-  }
+  vpc_security_group_ids = ["sg-05475defcc5748318"
 }
